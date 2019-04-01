a995d21cfe6b8b3968114b4b1a429eb01bc748ae
abstractMachina <abstractmachina@gmail.com>
2019-04-01 17:07:36 +0100

    First Commit

---
+++ Cyborg/Rhino/LoftSplitComponent.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace Cyborg.Rhino.NURBS
+{
+    public class LoftSplitComponent : GH_Component
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public LoftSplitComponent()
+          : base("Loft Split", "LS",
+              "Loft and split at tangents.",
+              "Cyborg", "NURBS")
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("1b7823cd-430c-4856-86c5-7367aa252329"); }
+        }
+    }
+}
